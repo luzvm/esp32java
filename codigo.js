8c16769edae9fd6975aa00150b7c63f52a720586
luzvm <135098366+luzvm@users.noreply.github.com>
2023-10-18 13:53:37 -0500

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -1,23 +1,18 @@
- const toggleButton = document.getElementById('toggleButton');
-const status = document.getElementById('status');
-let socket;
+ const chat = document.getElementById('chat');
+const messageInput = document.getElementById('messageInput');
+const sendButton = document.getElementById('sendButton');
+const socket = new WebSocket('ws://localhost:3000');
 
-toggleButton.addEventListener('click', () => {
-  if (!socket || socket.readyState === WebSocket.CLOSED) {
-    socket = new WebSocket('ws://localhost:3000');
+socket.onmessage = (event) => {
+  const message = event.data;
+  chat.innerHTML += `<p>${message}</p>`;
+  chat.scrollTop = chat.scrollHeight;
+};
 
-    socket.onopen = () => {
-      status.textContent = 'Conectado';
-      toggleButton.textContent = 'Desconectar';
-      toggleButton.style.backgroundColor = '#dc3545';
-    };
-
-    socket.onclose = () => {
-      status.textContent = 'Desconectado';
-      toggleButton.textContent = 'Conectar';
-      toggleButton.style.backgroundColor = '#007BFF';
-    };
-  } else {
-    socket.close();
+sendButton.addEventListener('click', () => {
+  const message = messageInput.value;
+  if (message) {
+    socket.send(message);
+    messageInput.value = '';
   }
 });
